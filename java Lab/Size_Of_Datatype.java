ffa27dcedb231d5cacd1dbf648232dc3aabeab5b
ShreyaGarud <shreyagarud4@gmail.com>
2024-08-30 22:59:16 +0530

    Add files via upload

---
+++ java Lab/Size_Of_Datatype.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Size_Of_Datatype {
+    public static void main(String[] args) {
+        
+    
+        System.out.println("Size of int :"+(Integer.SIZE/8)+"bytes");
+        
+        System.out.println("Size of float :"+(Float.SIZE/8)+"bytes");
+        
+        System.out.println("Size of double :"+(Double.SIZE/8)+"bytes");
+
+
+    }
+}
