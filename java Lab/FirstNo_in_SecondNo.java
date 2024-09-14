620acbbe7891eae557a4175189feb0d28532a2b7
ShreyaGarud <shreyagarud4@gmail.com>
2024-09-14 22:33:05 +0530

    Add files via upload

---
+++ java Lab/FirstNo_in_SecondNo.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class FirstNo_in_SecondNo
+{
+    public static void main(String[] args) 
+    {
+        Scanner s =new Scanner(System.in);
+
+       System.out.print("Enter Num :");
+       int Num1 = s.nextInt();
+
+        for(int i = 1 ; i <= 5 ; i++)
+        {
+            System.out.println(i);
+        }
+
+    }
+}
+   
+
